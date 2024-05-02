9a19aba86fd383b3628aa3a5f990b15c53272571
Abduali <dryden1309@gmail.com>
2024-05-02 22:43:50 +0500

    first commit

---
+++ src/main/java/com/company/founders/FoundersApplication.java
@@ -0,0 +1,13 @@
+package com.company.founders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoundersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoundersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/founders/FoundersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.founders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoundersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
